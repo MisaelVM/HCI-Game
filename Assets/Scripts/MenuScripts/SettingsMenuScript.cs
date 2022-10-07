12af3d07be4c523aea4072fbcf528f8ad94ca20c
MisaelVM <mvilchezmo@unsa.edu.pe>
2022-10-07 06:12:14 -0500

    Slider Volumen

--- Assets/Scripts/MenuScripts/SettingsMenuScript.cs
+++ Assets/Scripts/MenuScripts/SettingsMenuScript.cs
@@ -8,6 +8,8 @@ public class SettingsMenuScript : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public List<Image> audioWaves;
+
     public TMPro.TMP_Dropdown resDropdown;
 
     Resolution[] resolutions;
@@ -44,6 +46,10 @@ public class SettingsMenuScript : MonoBehaviour
     public void SetVolume(float volume) {
         Debug.Log(volume);
         //audioMixer.SetFloat("MainMixerVolume", volume);
+        float vol = volume; // Clamp volume
+        audioWaves[0].enabled = (vol > 0);
+        audioWaves[1].enabled = (vol > 100.0f / 3.0f);
+        audioWaves[2].enabled = (vol > 2 * 100.0f / 3.0f);
         audioMixer.SetFloat("MainMixerVolume", Mathf.Log10(volume) * 20);
     }
 
---
+++ Assets/Scripts/MenuScripts/VolumeScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeScript : MonoBehaviour
+{
+    public Slider slider;
+    public List<Image> audioWaves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
